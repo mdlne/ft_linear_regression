503685cb0bbf52c27be9d1edeb24838eb33e8e0e
Marine <delanoue.marine@hotmail.fr>
2025-06-26 08:45:08 +0200

    fix: minor issue

--- program1.py
+++ program1.py
@@ -3,11 +3,14 @@
 import csv
 
 
-def estimate_p(a, b):
+def estimate_p(a, b, min, max):
+    print(a, b, min, max)
     km = int(input("Comment de kilométrage a votre voiture ?"))
-    price = a * km + b
+    norm_km = (km - min) / (max - min)
+    denom_price = a * norm_km + b
+    norm_price = denom_price * (max - min) + min
     # print(f"Votre voiture est estimée au prix de {price}")
-    return price
+    return norm_price
 
 
 def read_csv_thetas(csv_file_name):
